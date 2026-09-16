afffc1c1b1ae734489683539d985894b6b97dd99
Loc <maaseenkout@gmail.com>
2026-09-16 19:28:45 +0700

    64.On Damage VFX

--- Assets/Scripts/Entity_Heath.cs
+++ Assets/Scripts/Entity_Heath.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 
 public class Entity_Heath : MonoBehaviour
 {
+    private Entity_VFX entityVfx;
+
     [SerializeField] protected float maxHp = 100;
     [SerializeField] protected bool isDead;
+
+    private void Awake()
+    {
+        entityVfx = GetComponent<Entity_VFX>();
+    }
 
     public virtual void TakeDamage(float damage, Transform dameDealer)
     {
         if (isDead)
             return;
 
+        entityVfx?.PlayOnDamageVfx();
         ReduceHp(damage);
     }
 
