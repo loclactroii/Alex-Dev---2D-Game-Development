5e4fd7f3426bfe20ad909a56bf8fec909b37b6c9
Loc <maaseenkout@gmail.com>
2026-09-17 13:35:10 +0700

    65. On Damage Knockback

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
@@ -18,6 +19,9 @@ public class Entity : MonoBehaviour
     [SerializeField] private Transform secondaryWallCheck;
     public bool groundDetected { get; private set; }
     public bool wallDetected { get; private set; }
+
+    private bool isKnocked;
+    private Coroutine knockbackCo;
 
     protected virtual void Awake()
     {
@@ -42,8 +46,30 @@ public class Entity : MonoBehaviour
         stateMachine.currentState.AnimationTrigger();
     }
 
+    public void ReceiveKnockback(Vector2 knockback, float duration)
+    {
+        if (knockbackCo != null)
+            StopCoroutine(knockbackCo);
+
+        StartCoroutine(KnockbackCo(knockback, duration));
+    }
+
+    private IEnumerator KnockbackCo(Vector2 knockback, float duration)
+    {
+        isKnocked = true;
+        rb.linearVelocity = knockback;
+
+        yield return new WaitForSeconds(duration);
+
+        rb.linearVelocity = Vector2.zero;
+        isKnocked = false;
+    }
+
     public void SetVelocity(float xVelocity, float yVelocity)
     {
+        if (isKnocked)
+            return;
+
         rb.linearVelocity = new Vector2(xVelocity, yVelocity);
         HandleFlip(xVelocity);
     }
--- Assets/Scripts/Entity_Heath.cs
+++ Assets/Scripts/Entity_Heath.cs
@@ -4,29 +4,47 @@ using UnityEngine;
 public class Entity_Heath : MonoBehaviour
 {
     private Entity_VFX entityVfx;
+    private Entity entity;
 
+    protected float currentHp;
     [SerializeField] protected float maxHp = 100;
     [SerializeField] protected bool isDead;
+
+    [Header("On Damage Knockback")]
+    [SerializeField] private Vector2 knockbackPower = new Vector2(1.5f, 2.5f);
+    [SerializeField] private Vector2 heavyKnockbackPower = new Vector2(7f, 7f);
+    [SerializeField] private float knockbackDuration = .2f;
+    [SerializeField] private float heavyKnockbackDuration = .5f;
+
+    [Header("On Heavy Damage")]
+    [SerializeField] private float heavyDamageThreshold = .3f;
 
     private void Awake()
     {
         entityVfx = GetComponent<Entity_VFX>();
+        entity = GetComponent<Entity>();
+
+        currentHp = maxHp;
     }
 
-    public virtual void TakeDamage(float damage, Transform dameDealer)
+    public virtual void TakeDamage(float damage, Transform damageDealer)
     {
         if (isDead)
             return;
 
+        Vector2 knockback = CalculateKnockback(damage, damageDealer);
+        float duration = CalculateDuration(damage);
+
+        entity?.ReceiveKnockback(knockback, duration);
         entityVfx?.PlayOnDamageVfx();
         ReduceHp(damage);
     }
 
     protected void ReduceHp(float damage)
     {
-        maxHp -= damage;
+        currentHp -= damage;
 
-        if (maxHp < 0)
+        if (currentHp <= 0)
             Die();
     }
 
@@ -35,4 +53,16 @@ public class Entity_Heath : MonoBehaviour
         isDead = true;
         Debug.Log("Entity died!");
     }
+
+    private Vector2 CalculateKnockback(float damage, Transform damageDealer)
+    {
+        int direction = transform.position.x > damageDealer.position.x ? 1 : -1;
+        Vector2 knockback = isHeavyDamage(damage) ? heavyKnockbackPower: knockbackPower;
+        knockback.x = knockback.x * direction;
+        return knockback;
+    }
+
+
+    private float CalculateDuration(float damage) => isHeavyDamage(damage) ? heavyKnockbackDuration : knockbackDuration;
+    private bool isHeavyDamage(float damage) => damage / maxHp > heavyDamageThreshold; 
 }
