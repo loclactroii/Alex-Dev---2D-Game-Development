f1d9cc1eb9db50eccdf1e1bf6566db18abed2ccf
Loc <maaseenkout@gmail.com>
2026-09-25 13:20:12 +0700

    75. Stat groups - 9mins

---
+++ Assets/Scripts/StatSystem/Stat_OffenceGroup.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Stat_OffenceGroup
+{
+    // Physical damage
+    public float damage;
+    public float critPower;
+    public float critChance;
+
+    // Elemental damage
+    public float fireDamage;
+    public float iceDamage;
+    public float lightningDamage;
+}
